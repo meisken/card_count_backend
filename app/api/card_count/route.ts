0c5f4cbaa10a1077ac5b6b870668ffa12c21fa86
meisken <meisken999@gmail.com>
2023-12-03 22:41:09 +0800

    test: don't use json file

--- app/api/card_count/route.ts
+++ app/api/card_count/route.ts
@@ -8,17 +8,18 @@ import { initializeJson } from '@/server_function/initializeJson';
 
 
 
-
+let data = {"playedCard":0,"multiplication":0};
 
 export async function GET(){
 
 
     //const cardCountData = await import("@/public/cardCountData.json", {assert: {type: "json"}});
     try{
-        if(!checkJsonFileExist()){
-            await initializeJson()
-        }
-        const data = await readJson()
+        // if(!checkJsonFileExist()){
+        //     await initializeJson()
+        // }
+        // const data = await readJson()
+        // console.log("card count get")
         return NextResponse.json({...data, status: 200 })
     }catch(err){
         return NextResponse.json({ status: 404, errorMessage: err })
@@ -28,17 +29,19 @@ export async function GET(){
 export async function POST(request: NextRequest){
    
     try{
-        if(!checkJsonFileExist()){
-            await initializeJson()
-        }
-        
-        const  isUpdated = await updateJsonData(request);
+        // if(!checkJsonFileExist()){
+        //     await initializeJson()
+        // }
+        const body = await request.json()
+
+        data = body
+        // const  isUpdated = await updateJsonData(request);
     
-  
-        return NextResponse.json({ isUpdated, status: 200 })
+        //console.log("card count post")
+        return NextResponse.json({ ...data, status: 200 })
     }catch(err){
         console.log(err)
-        return NextResponse.json({ isUpdated:false, status: 404, errorMessage: err })
+        return NextResponse.json({ /*isUpdated:false,*/ status: 404, errorMessage: err })
     }
  
 
