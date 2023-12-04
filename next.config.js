7dab43305ebe390e72a5e55984791b847c7f3808
meisken <meisken999@gmail.com>
2023-12-04 19:04:37 +0800

    allow accept

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
                     { key: "Access-Control-Allow-Credentials", value: "true" },
                     { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
                     { key: "Access-Control-Allow-Methods", value: "GET,DELETE" },
-                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, pragma, Cache-Control, cache" },
+                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, pragma, Cache-Control, cache, Accept-Request" },
                 ]
             }
         ]
